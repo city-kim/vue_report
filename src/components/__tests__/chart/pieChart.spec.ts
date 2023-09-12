135a3b094828bde5bd41a699130f7e951c193878
ctkim <nkdevil@naver.com>
2023-09-12 09:49:50 +0900

    fixed console error message
    - unit:test시 출력되는 에러메시지 지우기

--- src/components/__tests__/chart/pieChart.spec.ts
+++ src/components/__tests__/chart/pieChart.spec.ts
@@ -1,10 +1,14 @@
 import { nextTick } from 'vue'
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, vi } from 'vitest'
 import { mount, VueWrapper } from '@vue/test-utils'
 import PieChartVue from '@/components/Chart/pieChart.vue'
 
 import type { ComponentPublicInstance } from 'vue'
 type ChartWrapper<T> = VueWrapper<ComponentPublicInstance & T>
+
+vi.mock('vue-chartjs', () => ({
+  Chart: () => null
+}))
 
 describe('PieChartVue', () => {
 
