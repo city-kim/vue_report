89366dab87976dbaa2895b48638a2974d774c2e9
ctkim <nkdevil@naver.com>
2023-09-12 09:44:20 +0900

    decreaseByPercent, add test case
    - unit test의 progressFil -> progressLine으로 변경
    - decreaseByPercent 함수 추가 및 테스트케이스 추가
    - progressCircle의 unit test 추가

--- src/components/progressCircle.vue
+++ src/components/progressCircle.vue
@@ -1,14 +1,9 @@
 <script lang="ts" setup>
 import { ref, computed } from 'vue'
-import { getRate } from '@/util/number_converter'
+import { getRate, decreaseByPercent } from '@/util/number_converter'
 import { getCssVar } from '@/util/color'
 const props = defineProps({
-  width: {
-    type: String,
-    required: false,
-    default: '10rem'
-  },
-  height: {
+  size: {
     type: String,
     required: false,
     default: '10rem'
@@ -32,12 +27,11 @@ const svg = ref<SVGElement|null>(null) // svg element
 
 // props.percent가 있다면 그대로 없다면 비율계산
 const percent = computed(() => props.percent ? props.percent : getRate(props.compare, props.base))
-//  svg의 strokeDasharray 계산 svg의 크기 * 2 * Math.PI * 0.45(circle의 r값이 45%이기 때문에 0.45를 곱해준다)
-const strokeDasharray = computed(() => svg.value?.clientWidth ? Number((svg.value?.clientWidth * 2 * Math.PI * 0.45).toFixed(2)) : 0)
+// svg의 strokeDasharray 계산 svg의 크기 * 2 * Math.PI * 0.45(circle의 r값이 45%이기 때문에 0.45를 곱해준다)
+const strokeDasharray = computed(() => svg.value?.clientWidth ? Number((svg.value?.clientWidth * 0.45 * 2 * Math.PI).toFixed(2)) : 0)
 const strokeDashoffset = computed(() => {
-  // strokeDashoffset 계산 최대 100이 되도록 한다
-  const per = percent.value / 100
-  return strokeDasharray.value ? strokeDasharray.value * (1 - per) : 0
+  // strokeDashoffset 계산
+  return decreaseByPercent(strokeDasharray.value, percent.value)
 })
 
 </script>
@@ -45,8 +39,8 @@ const strokeDashoffset = computed(() => {
   <div class="progress-circle">
     <svg
       ref="svg"
-      :width="props.width"
-      :height="props.height"
+      :width="props.size"
+      :height="props.size"
     >
       <circle
         cx="50%"
@@ -59,7 +53,11 @@ const strokeDashoffset = computed(() => {
         cy="50%"
         r="45%"
         :stroke-width="strokeWidth"
-        :style="{ strokeDasharray: `${strokeDasharray}px`, strokeDashoffset: `${strokeDashoffset}px` }"
+        :style="{
+          strokeDasharray: strokeDasharray + 'px',
+          strokeDashoffset: strokeDashoffset + 'px',
+          stroke: props.color,
+        }"
       />
     </svg>
     <p>{{ percent }}%</p>
@@ -76,7 +74,6 @@ const strokeDashoffset = computed(() => {
       stroke: var(--color-bg-mute);
     }
     circle:last-child {
-      stroke: var(--c-blue);
       stroke-linecap: square;
       transition: stroke-dashoffset 1s ease-in-out;
     }
--- src/util/number_converter.ts
+++ src/util/number_converter.ts
@@ -2,7 +2,7 @@
  * from에서 to로 변하면 얼마나 증감한지 구하기
  * @param from number 비교할 대상
  * @param to number 비교될 값
- * @returns 전달된 값에대한 비율 (값이 하나라도 없으면 0)
+ * @returns 전달된 값에 대한 비율 (값이 하나라도 없으면 0)
  */
 const getPercent = (from?: number, to?: number) => {
   if (!from || !to) return 0
@@ -20,4 +20,15 @@ const getRate = (count?: number, totalCount?: number) => {
   return Math.floor((count / totalCount) * 100)
 }
 
-export { getPercent, getRate }
+/**
+ * totalValue값에 decrease퍼센트 만큼 감소한 값을 구하기
+ * @param totalValue number 전체 값
+ * @param decrease number 감소시킬 퍼센트
+ * @returns 전달된 전체 값에 대한 감소하고 남은 값 (값이 하나라도 없으면 0)
+ */
+const decreaseByPercent  = (totalValue?: number, decrease?: number) => {
+  if (!decrease || !totalValue) return 0
+  return totalValue * (1 - decrease / 100)
+}
+
+export { getPercent, getRate, decreaseByPercent }
