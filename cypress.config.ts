6c13d2da9f46f8dffde6ec3453d57319088604cf
ctkim <nkdevil@naver.com>
2023-10-12 15:14:19 +0900

    add github repo name
    - cypress.config의 baseUrl에 vue_report 추가
    - package scripts에도 추가 test:e2e:dev는 추가하지 않아도된다

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,10 +3,9 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
-    baseUrl: "http://localhost:4173",
+    baseUrl: "http://localhost:4173/vue_report",
     defaultCommandTimeout: 6000,
   },
-
   component: {
     devServer: {
       framework: "vue",
